688e136c2d77b9656573971c6ea26ed537dc8fa4
lekhins <alekseenkolesha@yandex.ru>
2024-02-12 17:34:38 +0300

    123

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -3,7 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
 
 
-def gendiff():
+def generate_diff():
     parser.add_argument(('first_file'))
     parser.add_argument(('second_file'))
     parser.add_argument('-f', '--format', help='set format of output')
@@ -12,7 +12,7 @@ def gendiff():
 
 
 def main():
-    return gendiff()
+    return generate_diff()
 
 
 if __name__ == '__main__':
