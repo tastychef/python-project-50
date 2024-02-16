e02f53f89029e22f6b907549901dd72864d4a599
a.alekseenko <alekseenkolesha@yandex.ru>
2024-02-16 05:13:34 +0300

    123

---
+++ gendiff/parser.py
@@ -0,0 +1,13 @@
+"""Module for parsing"""
+
+import json
+import yaml
+
+
+def parse(data, format_name):
+    if format_name == '.json':
+        return json.load(open(data))
+    if format_name in ('.yml', '.yaml'):
+        return yaml.safe_load(open(data))
+
+    raise ValueError('Unknown file format')
