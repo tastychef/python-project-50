8395714ad5a29c2afe6e587098c5fe9ad6217fdb
lekhins <alekseenkolesha@yandex.ru>
2024-02-20 16:00:37 +0300

    123

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,11 +7,5 @@ def generate_diff(file_path1, file_path2, format='stylish'):
         open_file(file_path1),
         open_file(file_path2)
     )
-
-    if format == 'plain':
-        return plain(differences)
-    elif format == 'json':
-        return json_(differences)
-    else:
-        return stylish(differences)
+    print(differences)
 
