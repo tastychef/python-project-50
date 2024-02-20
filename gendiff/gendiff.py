9b8157629c6ed20405aab14d7747b3783c425b20
lekhins <alekseenkolesha@yandex.ru>
2024-02-20 16:00:48 +0300

    123

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -2,7 +2,7 @@ from gendiff.open_file import open_file
 from gendiff.gasket import gasket
 
 
-def generate_diff(file_path1, file_path2, format='stylish'):
+def generate_diff(file_path1, file_path2):
     differences = gasket(
         open_file(file_path1),
         open_file(file_path2)
