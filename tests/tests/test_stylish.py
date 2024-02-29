e4ec7a891bcae15ef35b18426db05f3f868d2028
a.alekseenko <alekseenkolesha@yandex.ru>
2024-02-29 19:50:21 +0300

    123

--- tests/tests/test_stylish.py
+++ tests/tests/test_stylish.py
@@ -6,7 +6,7 @@ def test_stylish():
     flat_data1 = open_file('tests/fixtures/file1.json', '.json')
     flat_data2 = open_file('tests/fixtures/file2.json', '.json')
     box_diff = diff(flat_data1, flat_data2)
-    flat_result = open('tests/fixtures/box_json.txt').read()
+    box_result = open('tests/fixtures/box_json.txt').read()
 
 
-    assert get_diff_stylish(box_diff) == flat_result
+    assert get_diff_stylish(box_diff) == box_result
