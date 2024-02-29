3790ed6dad2bc72741b112d3f1ae90593626630f
a.alekseenko <alekseenkolesha@yandex.ru>
2024-02-29 19:54:14 +0300

    123

--- tests/tests/test_stylish.py
+++ tests/tests/test_stylish.py
@@ -3,9 +3,9 @@ from gendiff.tree import diff
 from gendiff.formatters.stylish import get_diff_stylish
 
 def test_stylish():
-    flat_data1 = open_file('tests/fixtures/file1.json', '.json')
-    flat_data2 = open_file('tests/fixtures/file2.json', '.json')
-    box_diff = diff(flat_data1, flat_data2)
+    box_data1 = open_file('tests/fixtures/file1.json', '.json')
+    box_data2 = open_file('tests/fixtures/file2.json', '.json')
+    box_diff = diff(box_data1, box_data2)
     box_result = open('tests/fixtures/box_stylish.txt').read()
 
 
